e30ddabc9657e9810964cf58e9d956b9839e474e
Taine <taine.reader@gmail.com>
2022-08-20 13:13:10 +1200

    . x3

--- node-app/index.js
+++ node-app/index.js
@@ -1,10 +1,30 @@
 const mqtt = require('mqtt')
+const express = require('express')
+
 const client  = mqtt.connect('mqtt://127.0.0.1')
+const app = express()
+
+app.use(express.json())
+
+
 
 client.on('connect', function () {
     console.log("connected")
-    client.publish('zigbee2mqtt/socket_1/set', '{"state": "ON"}')
+    // client.publish('zigbee2mqtt/socket_1/set', '{"state": "ON"}')
 })
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
+app.post('/socket_1', function (req, res) {
+    res.send(req.body)
+    console.log(req.body)
+    client.publish('zigbee2mqtt/socket_1/set', JSON.stringify(req.body)) 
+    console.log('command sent')
+})
+
+app.listen(3000)
 
 // client.on('message', function (topic, message) {
 //     // message is Buffer
