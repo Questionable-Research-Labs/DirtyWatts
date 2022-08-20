34d458b50803d8a762c36d69b5ea05e084c923b6
Taine <taine.reader@gmail.com>
2022-08-20 19:53:19 +1200

    . x8

--- node-app/index.js
+++ node-app/index.js
@@ -4,6 +4,7 @@ const https = require('https')
 const { response } = require('express')
 const { waitForDebugger } = require('inspector')
 const { resolveCaa } = require('dns')
+const { fips } = require('crypto')
 const client  = mqtt.connect('mqtt://127.0.0.1')
 const app = express()
 app.use(express.json())
@@ -11,7 +12,7 @@ app.use(express.json())
 
 function getDataFromAPI() {
     return new Promise((resolve, reject) => {
-        https.get('https://dirty-watts-api.host.qrl.nz/power_stations', (response) => {
+        https.get('https://dirty-watts-api.host.qrl.nz/live/power_stations', (response) => {
             let data = ''
             response.on('data', (chunk) => {
                 data += chunk
@@ -21,7 +22,7 @@ function getDataFromAPI() {
                 // console.log(data)
                 try {
                 parsed_data = JSON.parse(data);
-                console.log(JSON.stringify(parsed_data))
+                //console.log(JSON.stringify(parsed_data))
                 resolve(parsed_data);
                 } catch (e) {
                     reject(e)
@@ -46,18 +47,45 @@ function update() {
                 let power_type = power_types[key];
                 total_generation_mw += power_type.generation_mw;
             }
-            console.log(total_generation_mw)
+
+            console.log("====================")
+            console.log("Total Power Generation: " + total_generation_mw + " MW")
 
             let bad_generation_mw = 0   // total up non-renewables power usage
-            bad_generation_mw = power_types.coal.generation_mw + power_types.gas.generation_mw + power_types.liquid.generation_mw
-            console.log(bad_generation_mw)
+            bad_generation_mw = power_types.coal.generation_mw + power_types.gas.generation_mw + power_types.diesel.generation_mw
+            console.log("Non-Renewable Power: " + bad_generation_mw + " MW")
             
-            percent_fossil = bad_generation_mw / total_generation_mw  // calculate % non-renewable
-            console.log(percent_fossil)
+            percent_fossil = 100 * (bad_generation_mw / total_generation_mw)  // calculate % non-renewable
+            console.log("Percentage Non-Renewable: " + percent_fossil + "%")
+
+            // COAL = 1073 grams co2 per kWh
+            // GAS = 410 grams co2 per kWh
+            // COGEN = 249 grams co2 per kWh
+            // GEO = 90 grams co2 per kWh
+
+            // WEIGHTED PERCENTAGE
+            let weighted_bad_generation = 0
+            weighted_bad_generation = power_types.coal.generation_mw * 10.73 + power_types.gas.generation_mw * 4.10 + power_types.geothermal.generation_mw * 0.90 + power_types.co_gen.generation_mw * 2.49
+            console.log("Weighted Bad Generation: " + weighted_bad_generation)
 
 
             // UPDATE LIGHT
-            client.publish('zigbee2mqtt/lamp_rgb_1/set', '{"state": "ON", "color": {"r": 255, "g": 255, "b": 0}}') 
+
+            if(percent_fossil > 20){
+                percent_fossil = 20
+            }
+            let fossil_amount = 0 
+            fossil_amount = percent_fossil / 20
+            console.log("Fossil: " + fossil_amount + " (% / 20)")
+            let red_value = 0
+            let green_value = 0
+            red_value = Math.round(255 * fossil_amount)
+            green_value = Math.round(255 - (255 * fossil_amount))
+            console.log("Value For Lamp: " + red_value + " " + green_value, + " " + 0 + " (RGB)")
+            
+            let lamp_info = {"state": "ON", "color": {"r": red_value, "g": green_value, "b": 0}}
+
+            client.publish('zigbee2mqtt/lamp_rgb_1/set', lamp_info) 
 
 
         }).catch(console.error)
@@ -65,7 +93,7 @@ function update() {
 
 setInterval(() => {
     update()
-}, 1000 * 60)
+}, 1000 * 5)
 
 update()
 
